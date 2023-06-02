bf8e2a70c6d70374a350c05627dc5ee0a73604a1
byun <byun@naver.com>
2023-06-02 11:08:59 +0900

    3rd commit

--- HelloTest.java
+++ HelloTest.java
@@ -1,6 +1,6 @@
 public class HelloTest {
    public static void main(String[] args) {
       System.out.println("Hello Git!!");
-      System.out.println("plus!!!!");
+      int sum = a + b;
    }
 }
